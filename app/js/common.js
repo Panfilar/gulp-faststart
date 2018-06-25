6ee4ebc4530188b7f0724e9366930adb21790d3e
Panfilar <artem1787@ya.ru>
2018-06-25 16:13:32 +0300

    промежуточный

--- app/js/common.js
+++ app/js/common.js
@@ -1,27 +1,12 @@
 $(document).ready(function () {
-    $('.border-wrapper').addEventListener("mouseout").on('click', function () {
-       /* var text = $(this).find(h3).text().trim();
-        var all_text = $('#type-of-work').val();
-        var all_text_arr = all_text ? all_text.split(',') : [];*/
-        if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
-            /*var tmp = [];
-            if (text) {
-                $.each(all_text_arr, function (key, value) {
-                    if (value.trim()) {
-                        if (text != value.trim()) {
-                            tmp.push(value.trim());
-                        }
-                    }
-                })
-                $('#type-of-work').val(tmp.join(','));
-            }*/
-        } else {
+    $('.border-wrapper').on('click', function () {
+        $(this).toggleClass('clicked');
+    });
+    $('.border-wrapper').mouseleave(function () {
+        if ($(this).hasClass('clicked')) {
             $(this).addClass('selected');
-           /* if (text) {
-                all_text_arr.push(text);
-                $('#type-of-work').val(all_text_arr.join(','));
-            }*/
+        } else {
+            $(this).removeClass('selected');
         }
     });
-})
+});
