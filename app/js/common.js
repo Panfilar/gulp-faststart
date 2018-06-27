9cb9a60bb79f423421e5aaa0c86c4b35fd71d0d9
Panfilar <artem1787@ya.ru>
2018-06-27 16:39:25 +0300

    частично дописал JS

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,34 @@
 $(document).ready(function () {
+    var textDef = 'Чего сидишь? Порадуй котэ, <span class="element__buy">купи</span>';
+    var textSelect = 'Печень утки разварная с артишоками.';
+
+
+
+
+
     $('.element__buy').on('click', function () {
         $(this).parents('.element').toggleClass('selected');
         $(this).parents('.element').toggleClass('clicked');
     });
 
     $('.element__wrapper').on('click', function () {
-        $(this).parents('.element').toggleClass('clicked');
+        $(this).parent('.element').toggleClass('clicked');
+        $(this).closest(".element").children('p.text').toggleText(textDef, textSelect);
+
     });
+
+    $('.element.disabled').on('click', function () {
+        var defText=$(this).children('p.text');
+        $(this).removeClass('clicked');
+        $(this).closest(".element").children('p.text').html('Печалька, ' + $(this).find('.element__key-title').html() + ' закончился.');
+    });
+    
+    $('.element.disabled').each(function () {
+        $(this).find('.text').html('Печалька, ' + $(this).find('.element__key-title').html() + ' закончился.');
+
+    });
+
+
     $('.element__wrapper').mouseleave(function () {
         if ($(this).parents('.element').hasClass('clicked')) {
             $(this).parents('.element').addClass('selected');
@@ -15,5 +37,22 @@ $(document).ready(function () {
         }
     })
 
+
+});
+
+jQuery.fn.extend({
+    toggleText: function (a, b) {
+        var that = this;
+        if (that.html() != a && that.html() != b) {
+            that.html(a);
+        }
+        else if (that.html() == a) {
+            that.html(b);
+        }
+        else if (that.html() == b) {
+            that.html(a);
+        }
+        return this;
+    }
 });
 
